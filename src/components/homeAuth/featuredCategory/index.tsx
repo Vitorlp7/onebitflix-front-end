227ea79272ff887f08787047b528048ee76bb9ec
Vitorlp7 <vitor09lr@gmail.com>
2025-05-01 16:07:32 -0300

    Criação dos elementos da seção de destaque, do seu estilo visual - Seção de destaque

---
+++ src/components/homeAuth/featuredCategory/index.tsx
@@ -0,0 +1,28 @@
+import useSWR from "swr";
+import styles from "../../../../styles/slideCategory.module.scss";
+import courseService from "../../../services/courseService";
+import SlideComponent from "../../common/slideComponent";
+
+const FeaturedCategory = () => {
+  const { data, error } = useSWR(
+    "/featured",
+    courseService.getFeaturedCourses
+  );
+
+  if (error) return error;
+  if (!data)
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+
+    return (
+        <>
+            <p className={styles.titleCategory}>Em destaque</p>
+            <SlideComponent course={data.data}/>
+        </>
+    )
+};
+
+export default FeaturedCategory
