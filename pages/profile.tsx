761ea2a30662320e9f8c4face5e2345f7c5e1d83
Vitorlp7 <vitor09lr@gmail.com>
2025-05-01 22:07:28 -0300

    Criação dos elementos da página de informações do usuário, do seu estilo visual e também da sua responsividade - Profile (Usuário - Visual)

---
+++ pages/profile.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import styles from "../styles/profile.module.scss";
+import UserForm from "../src/components/profile/user";
+import HeaderAuth from "../src/components/common/headerAuth";
+import { Button, Col, Container, Row } from "reactstrap";
+import Footer from "../src/components/common/footer";
+
+const UserInfo = () => {
+  return (
+    <>
+      <Head>
+        <title>OneBitFlix - Meus Dados</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      </Head>
+      <main>
+        <div className={styles.header}>
+          <HeaderAuth />
+        </div>
+        <Container className="py-5">
+          <p className={styles.title}>Minha conta</p>
+          <Row className="pt-3 pb-5">
+            <Col md={4} className={styles.btnColumn}>
+              <Button className={styles.renderForm}>Dados Pessoais</Button>
+              <Button className={styles.renderForm}>Senha</Button>
+            </Col>
+            <Col md>
+              <UserForm />
+            </Col>
+          </Row>
+        </Container>
+        <div className={styles.footer}>
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default UserInfo;
---
+++ src/components/profile/user/index.tsx
@@ -0,0 +1,81 @@
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import styles from "../../../../styles/profile.module.scss";
+
+const UserForm = () => {
+  return (
+    <>
+      <Form className={styles.form}>
+        <div className={styles.formName}>
+          <p className={styles.nameAbbreviation}>NT</p>
+          <p className={styles.userName}>NAME TEST</p>
+        </div>
+        <div className={styles.memberTime}>
+          <img src="/profile/iconUserAccount.svg" alt="iconProfile" className={styles.memberTimeImg} />
+          <p className={styles.memberText}>
+            Membro desde <br /> 20 de Abril de 2020
+          </p>
+        </div>
+        <hr />
+        <div className={styles.inputFlexDiv}>
+          <FormGroup>
+            <Label for="firstName" className={styles.label}>Nome</Label>
+            <Input
+              name="firstName"
+              type="text"
+              id="firstName"
+              placeholder="Qual o seu primeiro nome?"
+              required
+              maxLength={20}
+              className={styles.inputFlex}
+              value={"Name"}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="lastName" className={styles.label}>Sobrenome</Label>
+            <Input
+              name="lastName"
+              type="text"
+              id="lastName"
+              placeholder="Qual o seu último nome?"
+              required
+              maxLength={20}
+              className={styles.inputFlex}
+              value={"test"}
+            />
+          </FormGroup>
+        </div>
+        <div className={styles.inputNormalDiv}>
+          <FormGroup>
+            <Label for="phone" className={styles.label}>WhatsApp / Telegram</Label>
+            <Input
+              name="phone"
+              type="text"
+              id="phone"
+              placeholder="(xx) 9xxxx-xxxx?"
+              required
+              className={styles.input}
+              value={"+55 (21) 9999-9999"}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email" className={styles.label}>E-mail</Label>
+            <Input
+              name="email"
+              type="text"
+              id="email"
+              placeholder="Digite o seu email"
+              required
+              className={styles.input}
+              value={"teste@gmail.com"}
+            />
+          </FormGroup>
+          <Button className={styles.formBtn} outline type="submit">
+            Salvar Alterações
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
+};
+
+export default UserForm;
