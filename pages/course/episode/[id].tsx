e3f2206655b98447273d3a13c23dfb536246b570
Vitorlp7 <vitor09lr@gmail.com>
2025-05-07 23:34:04 -0300

    Front-end reestruturado para que seja possível pular automaticamente de vídeo quando finalizado e também recuperar informações de quantos segundos foram assistidos de cada episódio baseado no usuário logado

--- pages/course/episode/[id].tsx
+++ pages/course/episode/[id].tsx
@@ -2,17 +2,55 @@ import { useRouter } from "next/router";
 import styles from "../../../styles/episodePlayer.module.scss";
 import Head from "next/head";
 import HeaderGeneric from "../../../src/components/common/headerGeneric";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import courseService, { CourseType } from "../../../src/services/courseService";
 import PageSpinner from "../../../src/components/common/spinner";
 import { Button, Container } from "reactstrap";
 import ReactPlayer from "react-player";
+import watchEpisodeService from "../../../src/services/episodeService";
 
 const EpisodePlayer = function () {
   const router = useRouter();
   const [course, setCourse] = useState<CourseType>();
+  const [isReady, setIsReady] = useState(false);
   const episodeOrder = parseFloat(router.query.id?.toString() || "");
+  const episodeId = parseFloat(router.query.episodeid?.toString() || "");
   const courseId = router.query.courseid?.toString() || "";
+
+  const [getEpisodeTime, setGetEpisodeTime] = useState(0);
+  const [episodeTime, setEpisodeTime] = useState(0);
+
+  const playerRef = useRef<ReactPlayer>(null);
+
+  const handleGetEpisodeTime = async () => {
+    const res = await watchEpisodeService.getWatchTime(episodeId);
+
+    if (res.data !== null) {
+      setGetEpisodeTime(res.data.seconds);
+    }
+  };
+
+  const handleSetEpisodeTime = async () => {
+    await watchEpisodeService.setWatchTime({
+      episodeId: episodeId,
+      seconds: Math.round(episodeTime),
+    });
+  };
+
+  useEffect(() => {
+    handleGetEpisodeTime();
+  }, [router]);
+
+  const handlePlayerTime = () => {
+    playerRef.current?.seekTo(getEpisodeTime);
+    setIsReady(true);
+  };
+
+  if (isReady === true) {
+    setTimeout(() => {
+      handleSetEpisodeTime();
+    }, 1000 * 3);
+  }
 
   const getCourse = async () => {
     if (typeof courseId !== "string") return;
@@ -23,20 +61,25 @@ const EpisodePlayer = function () {
       setCourse(res.data);
     }
   };
+  if (course?.episodes === undefined) return <PageSpinner />;
 
   const handleLastEpisde = () => {
-    router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}`);
+    router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}&episodeid=${episodeId - 1}`);
   };
 
   const handleNextEpisde = () => {
-    router.push(`/course/episode/${episodeOrder + 1}?courseid=${course?.id}`);
+    router.push(`/course/episode/${episodeOrder + 1}?courseid=${course?.id}&episodeid=${episodeId + 1}`);
   };
 
   useEffect(() => {
     getCourse();
   }, [courseId]);
 
-  if (course?.episodes === undefined) return <PageSpinner />;
+  if (episodeOrder + 1 < course?.episodes?.length) {
+    if (Math.round(episodeTime) === course.episodes[episodeOrder].secondsLong) {
+      handleNextEpisde();
+    }
+  }
 
   return (
     <>
@@ -55,6 +98,11 @@ const EpisodePlayer = function () {
                 course.episodes[episodeOrder].videoUrl
               }&token=${sessionStorage.getItem("onebitflix-token")}`}
               controls
+              ref={playerRef}
+              onStart={handlePlayerTime}
+              onProgress={(progress) => {
+                setEpisodeTime(progress.playedSeconds);
+              }}
             />
           )}
           <div className={styles.episodeButtonDiv}>
@@ -73,9 +121,7 @@ const EpisodePlayer = function () {
               <img src="/episode/iconArrowRight.svg" alt="setaDireita" className={styles.arrowImg} />
             </Button>
           </div>
-          <p className="text-center py-4">
-            {course.episodes[episodeOrder].synopsis}
-          </p>
+          <p className="text-center py-4">{course.episodes[episodeOrder].synopsis}</p>
         </Container>
       </main>
     </>
